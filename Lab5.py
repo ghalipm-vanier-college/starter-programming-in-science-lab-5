72ec69c170e620d76f07eff4cd6f391db26cc521
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-02-24 16:49:01 -0500

    Create Lab5.py
    Lab 5 added

---
+++ Lab5.py
@@ -0,0 +1,27 @@
+# Example for n = 5:
+# *****
+# *   *
+# *   *
+# *   *
+# *****
+def hollow_square(n):
+    return ""
+
+# 1
+# 12
+# 123
+# 1234
+def number_pattern(n):
+    return ""
+
+# Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
+def sum_of_natural_numbers(n):
+    return ""
+
+# Example for n = 4:
+#    *
+#   ***
+#  *****
+# *******
+def centered_star_pyramid(n):
+    return ""
